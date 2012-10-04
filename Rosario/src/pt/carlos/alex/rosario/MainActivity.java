685d401b5f8e0a43494ee3c766063c974fec4f65
alexandre <alexandre@alex-i3>
2012-10-04 20:23:50 +0100

    Versão inicial

---
+++ Rosario/src/pt/carlos/alex/rosario/MainActivity.java
@@ -0,0 +1,64 @@
+package pt.carlos.alex.rosario;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.OptionsMenu;
+import com.googlecode.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
+@OptionsMenu(R.menu.activity_main)
+public class MainActivity extends SherlockActivity {
+
+	@ViewById(R.id.dia_semana)
+	TextView dia_semana;
+
+	Date dia;
+	Calendar calendario;
+	String d_semana;
+
+	@AfterViews
+	void updateTextWithDate() {
+//		this.dia = new Date();
+
+//		calendario = new GregorianCalendar();
+//		calendario.setTime(dia);
+		calendario = Calendar.getInstance();
+
+		switch (calendario.get(Calendar.DAY_OF_WEEK)) {
+		case Calendar.SUNDAY:
+			d_semana = "Domingo - Mistérios Gloriosos (da Glória)";
+			break;
+		case Calendar.MONDAY:
+			d_semana = "2ª Feira - Mistérios Gozosos (da Alegria)";
+			break;
+		case Calendar.TUESDAY:
+			d_semana = "3ª Feira - Mistérios Dolorosos (da Dor)";
+			break;
+		case Calendar.WEDNESDAY:
+			d_semana = "4ª Feira - Mistérios Gloriosos (da Glória)";
+			break;
+		case Calendar.THURSDAY:
+			d_semana = "5ª Feira - Mistérios Luminosos (da Luz)";
+			break;
+		case Calendar.FRIDAY:
+			d_semana = "6ª Feira - Mistérios Dolorosos (da Dor)";
+			break;
+		case Calendar.SATURDAY:
+			d_semana = "Sábado - Mistérios Gozosos (da Alegria)";
+			break;
+		default:
+			d_semana = "Dia da semana errado";
+			break;
+		}
+
+		dia_semana.setText("Dia da semana: " + d_semana);
+
+	}
+
+}
